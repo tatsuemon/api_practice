c7344f9e5df7e45e7b2404dcc2917b94829f3613
Tatsuemon <i10mann-110@ezweb.ne.jp>
2020-02-29 22:01:29 +0900

    fix migration

--- db/migrate/20200229130055_create_recipes.rb
+++ db/migrate/20200229130055_create_recipes.rb
@@ -1,4 +1,4 @@
-class CreateRecipe < ActiveRecord::Migration[6.0]
+class CreateRecipes < ActiveRecord::Migration[6.0]
   def change
     create_table :recipes do |t|
       t.string :title, null: false
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_29_115429) do
+ActiveRecord::Schema.define(version: 2020_02_29_130055) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title", null: false
