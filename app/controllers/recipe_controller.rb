6e783e9facbbfbafc83b4c3d81e33c5148903482
Tatsuemon <i10mann-110@ezweb.ne.jp>
2020-02-29 21:37:37 +0900

    make it better

---
+++ app/controllers/recipe_controller.rb
@@ -0,0 +1,41 @@
+class RecipeController < ApplicationController
+    def index
+        recipes = Recipe.all()
+        render json: {recipes: recipes}
+    end
+
+    def show
+        recipe = Recipe.find(params[:id])
+        render jdon: {message: "Recipe details by id", recipe: recipe}
+    end
+
+    def create
+        recipe = Recipe.new(post_params)
+        if recipe.save
+            render json: {message: 'Recipe successfully created!', recipe: recipe}
+        else
+            render json: {message: 'Recipe create failed!', required: 'title, making_time, serves, ingredients, cost'}
+        end
+    end
+
+    def update
+        recipe = Recipe.find(params[:id])
+        recipe = recipe.update(post_params)
+        render json: {message: 'Recipe successfully updated!', recipe: recipe}
+    end
+
+    def destroy
+        recipe = Recipe.find(params[:id])
+        if recipe.destroy
+            render json: {message: 'Recipe successfully removed!'}
+        else
+            render json: {message: 'No Recipe found'}
+        end
+    end
+    private
+
+    def post_params
+        params.require(:recipe).permit(:title, :making_time, :ingredients, :cost)
+    end
+
+end
---
+++ db/migrate/20200229115429_recipe.rb
@@ -0,0 +1,12 @@
+class Recipe < ActiveRecord::Migration[6.0]
+  def change
+    create_table :recipes do |t|
+      t.string :title, null: false
+      t.string :making_time, null: false
+      t.string :serves, null: false
+      t.string :ingredients, null: false
+      t.integer :cost, null: false
+      t.timestamps
+    end
+  end
+end
